8430bc1f0596c47da5c09b21758bea30186defb5
Todd Wells <todd.wells@ironnetcybersecurity.com>
2022-03-10 10:52:57 -0800

    initial commit

---
+++ ch1/divzero.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 1 / 0
+	fmt.Println(a)
+}
---
+++ ch1/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	x_y_z := 123
+	fmt.Println("Hello, world!")
+	fmt.Println(x_y_z)
+}
